ca62e8253f6bbe64f99b1b5da9a634edbeafdb60
PYYRQ <165933124+PYYRQ@users.noreply.github.com>
2024-04-07 16:00:07 +0300

    Update رفع مشرف.py

--- AarohiX/plugins/play/رفع مشرف.py
+++ AarohiX/plugins/play/رفع مشرف.py
@@ -1,69 +1,152 @@
+
+ ###### ###### ###### #####   ################
+   ##   ##       ##  ##   ##  #######
+   ##   ##       ##  ##   ##  ###########
+   ##   #####    ##  ##   ##  #########
+   ##   ##       ##  ##   ##  ############
+   ##   ##       ##  ##   ##  #####𝗔𝗵𝗺𝗲𝗱 𝗧𝗲𝘁𝗼
+   ##   ######   ##   ##### ################
+
+
+from pyrogram import Client, filters
+from pyrogram import enums
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus , ChatType
+from pyrogram.types import ChatPermissions, ChatPrivileges
 from AarohiX import app
-from AarohiX.misc import SUDOERS
-from pyrogram import * 
-from pyrogram.types import *
-from AarohiX.utils.admin_check import admin_filter
+import asyncio
 
-strict_txt = [
-    "ᴅᴏsᴛ ᴋɪ ɢᴀᴀɴᴅ ɴʜɪ ᴍᴀᴀʀ sᴋᴛᴀ.",
-    "sᴀᴄʜ ᴍᴇ? ᴀᴘɴᴇ ᴅᴏsᴛ ᴋɪ ɢᴀᴀɴᴅ ᴍᴀᴀʀ ʟᴏᴏɴ.",
-    "ᴍᴀɪɴ ᴀᴘɴᴇ ʙᴇsᴛ ғʀɪᴇɴᴅ ᴋᴀ ʟᴜɴᴅ ɴʜɪ ᴋᴀᴀᴛ sᴋᴛᴀ.",
-    "ᴍᴀɪɴ ᴜsᴇ ᴊᴀᴀɴᴛᴀ ʜᴏᴏɴ. ᴍᴜᴊʜᴇ sᴀᴍᴀᴊʜɴᴇ ᴋᴀ ᴘʀʏᴀssʜ ᴋʀᴏ ɪsᴋɪ ᴍᴀᴀʀɪ ᴛᴏ ʀᴀɴᴅᴡᴀ ᴍᴀʀʀ ᴊᴀʏᴇɢᴀ."
-]
 
-promote = ["رفع مشرف", "adminship"]
-fullpromote = ["مشرف كامل", "fadmin"]
-demote = ["تنزيل مشرف", "lelo"]
 
-# ========================================= #
 
-@app.on_message(filters.command(["رفع مشرف", "تنزيل مشرف", "رفع مشرف كامل"]) & admin_filter)
-async def handle_admin_commands(_, message):
-    reply = message.reply_to_message
-    chat_id = message.chat.id
+welcome_enabled = True
+
+
+
+
+
+
+@app.on_chat_member_updated()
+async def welcome(client, chat_member_updated):
+    if not welcome_enabled:
+        return
     
-    if not reply:
-        return await message.reply(random.choice(strict_txt))
+    if chat_member_updated.new_chat_member.status == ChatMemberStatus.BANNED:
+        kicked_by = chat_member_updated.new_chat_member.restricted_by
+        user = chat_member_updated.new_chat_member.user
+        
+        if kicked_by is not None and kicked_by.is_self:
+            messagee = f"↢ المستخدم {user.username} ({user.first_name}) تم طرده من الدردشة بواسطة البوت"
+        else:
+            if kicked_by is not None:
+                message = f"منع التصفية التـلقائي 🛡️\n\n↢ المستخدم : [{kicked_by.first_name}](tg://user?id={kicked_by.id}) تـم أنزله من قائمه الادمنيه\n↢ السبب : حاول تصفيه مجموعتك وطرد العضو : [{user.first_name}](tg://user?id={user.id})"
+                try:
+                    await client.ban_chat_member(chat_member_updated.chat.id, kicked_by.id)
+                except Exception as e:
+                    message += f"\n\nلازم يكون المشرف مرفوع من البوت عشان أقدر أنزله من الإشراف \nلـ معرفه كيفيه رفع مشرف : قم بعمل ريبلي علي الشخص المحدد واكتب رفع مشرف"
+            else:
+                message = f"↢ المستخدم {user.username} ({user.first_name}) - تم طرده من الدردشة"
+            
+            
+        
+        await client.send_message(chat_member_updated.chat.id, message)
 
-    user_id = reply.from_user.id
-    command = message.command[0].lower()
 
-    if command in promote:
-        await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-            can_change_info=False,
-            can_invite_users=True,
-            can_delete_messages=True,
-            can_restrict_members=False,
-            can_pin_messages=True,
-            can_promote_members=False,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-        ))
-        await message.reply("↢ أبشر رفعته مُشرف .")
 
-    elif command in demote:
-        await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-            can_change_info=False,
-            can_invite_users=False,
-            can_delete_messages=False,
-            can_restrict_members=False,
-            can_pin_messages=False,
-            can_promote_members=False,
-            can_manage_chat=False,
-            can_manage_video_chats=False,
-        ))
-        await message.reply("↢ أبشر نزلته مشرف")
 
-    elif command in fullpromote:
-        await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-            can_change_info=True,
-            can_invite_users=True,
-            can_delete_messages=True,
-            can_restrict_members=True,
-            can_pin_messages=True,
-            can_promote_members=True,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-        ))
-        await message.reply("↢ تم رفعه مشرف ♡")
-                                      
+@app.on_message(filters.command(["رفع مشرف"], "") & filters.channel)
+def promote_c_admin(client, message):
+    if message.reply_to_message and message.reply_to_message.from_user:
+        target = message.reply_to_message.from_user.id
+        user_id = str(target)
+    elif message.reply_to_message is None:
+        target = message.text.split()[2]
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("↢ مين هذا ما قدرت أتعرف على حسابه؟")
+            return
+    else:
+        target = message.text.split()[1].strip("@")
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("↢ مين هذا ما قدرت أتعرف على حسابه؟")
+            return
+
+    
+    ToM= ChatPrivileges(
+                    can_manage_chat=True,
+                    can_delete_messages=True,
+                    can_manage_video_chats=True,
+                    can_restrict_members=True,
+                    can_promote_members=False,
+                    can_change_info=False,
+                    can_post_messages=True,
+                    can_edit_messages=True,
+                    can_invite_users=True,
+                    can_pin_messages=False,
+                    is_anonymous=False
+                )
+    chat_id = message.chat.id
+    client.promote_chat_member(chat_id, user_id, ToM)
+    message.reply(f"↢ الحلو {user_id} صار مشرف...")
+    
+
+
+
+@app.on_message(filters.command(["رفع مشرف"], "") & filters.group)
+def promote_g_admin(client, message):
+    if message.reply_to_message and message.reply_to_message.from_user:
+        target = message.reply_to_message.from_user.id
+        user_id = str(target)
+    elif message.reply_to_message is None:
+        target = message.text.split()[2]
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+    else:
+        target = message.text.split()[1].strip("@")
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("↢ مين هذا ما قدرت أتعرف على حسابه؟")
+            return
+
+    tom_id = message.from_user.id
+    chat_id = message.chat.id
+    ToM= ChatPrivileges(
+                    can_manage_chat=True,
+                    can_delete_messages=True,
+                    can_manage_video_chats=True,
+                    can_restrict_members=True,
+                    can_promote_members=False,
+                    can_change_info=False,
+                    can_post_messages=False,
+                    can_edit_messages=False,
+                    can_invite_users=True,
+                    can_pin_messages=True,
+                    is_anonymous=False
+                )
+    tooom = client.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)
+    for tom in tooom:
+    	if tom.user.id == tom_id and (tom.status == enums.ChatMemberStatus.OWNER or tom.status == enums.ChatMemberStatus.ADMINISTRATOR):
+    		client.promote_chat_member(chat_id, user_id, ToM)
+    		message.reply(f"↢ الحلو {user_id} صار مشرف....")
+    		
+    		
+    		
+ ###### ###### ###### #####   ################
+   ##   ##       ##  ##   ##  #######
+   ##   ##       ##  ##   ##  ###########
+   ##   #####    ##  ##   ##  #########
+   ##   ##       ##  ##   ##  ############
+   ##   ##       ##  ##   ##  #####𝗔𝗵𝗺a𝗱 𝗧𝗲𝘁𝗼
+   ##   ######   ##   ##### ################
+    		
+    
