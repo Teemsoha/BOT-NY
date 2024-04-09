69987cdf7d16a8104088397c0172dac4dd597aab
PYYRQ <165933124+PYYRQ@users.noreply.github.com>
2024-04-09 23:56:38 +0300

    Update TotalMembers.py

--- AarohiX/plugins/Management/TotalMembers.py
+++ AarohiX/plugins/Management/TotalMembers.py
@@ -4,7 +4,7 @@ import csv
 from pyrogram import Client, filters
 from AarohiX import app
 
-@app.on_message(filters.command("إحصائيات", "الإحصائيات")) & admin_filter)
+@app.on_message(filters.command("إحصائيات" ,"الإحصائيات")) & admin_filter)
 def user_command(client, message):
     
     chat_members = app.get_chat_members(message.chat.id)
